828af506f8053585642a2321bd619077db8a1f11
JovanneSousa <134342380+JovanneSousa@users.noreply.github.com>
2025-11-04 10:01:33 -0400

    Update API base URL to new endpoint
    Corrigindo o endereço da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -37,7 +37,7 @@ type PurchasePayload = {
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://ebac-fake-api.vercel.app/api/efood/'
+    baseUrl: 'https://api-ebac.vercel.app/api/efood/'
   }),
   endpoints: (builder) => ({
     getRestaurantes: builder.query<Restaurante[], void>({
